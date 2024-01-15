9fa24c817ba1f7e5cf48f6eea1d232de89d867bc
Roy97 <s224021028@deakin.edu.au>
2024-01-15 12:56:30 +1100

    added database schemas

---
+++ models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    usersModel: require("./users.model"),
+    booksModel: require("./books.model"),
+    requestsModel: require("./requests.model"),
+    reservationsModel: require("./reservations.model")
+}
